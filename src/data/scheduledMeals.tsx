4be70d6af7669119b7d82ad39d5e53c51c771bc2
11arn11 <36072776+11arn11@users.noreply.github.com>
2021-09-12 18:40:25 +0200

    update 2021-09-12

--- src/data/scheduledMeals.tsx
+++ src/data/scheduledMeals.tsx
@@ -6,25 +6,25 @@ export const scheduledMeals = [
 
   { id: 1, date: "2021-09-14", mealType: "breakfast", recipesId: [1] },
   { id: 1, date: "2021-09-14", mealType: "morning_snack", recipesId: [11] },
-  { id: 1, date: "2021-09-14", mealType: "lunch", recipesId: [32] },
+  { id: 1, date: "2021-09-14", mealType: "lunch", recipesId: [30, 33] },
   { id: 1, date: "2021-09-14", mealType: "afternoon_snack", recipesId: [12] },
-  { id: 1, date: "2021-09-14", mealType: "dinner", recipesId: [] },
+  { id: 1, date: "2021-09-14", mealType: "dinner", recipesId: [34] },
 
   { id: 1, date: "2021-09-15", mealType: "breakfast", recipesId: [1] },
   { id: 1, date: "2021-09-15", mealType: "morning_snack", recipesId: [11] },
-  { id: 1, date: "2021-09-15", mealType: "lunch", recipesId: [32] },
+  { id: 1, date: "2021-09-15", mealType: "lunch", recipesId: [29, 30] },
   { id: 1, date: "2021-09-15", mealType: "afternoon_snack", recipesId: [12] },
-  { id: 1, date: "2021-09-15", mealType: "dinner", recipesId: [] },
+  { id: 1, date: "2021-09-15", mealType: "dinner", recipesId: [35] },
 
   { id: 1, date: "2021-09-16", mealType: "breakfast", recipesId: [1] },
   { id: 1, date: "2021-09-16", mealType: "morning_snack", recipesId: [11] },
-  { id: 1, date: "2021-09-16", mealType: "lunch", recipesId: [32] },
+  { id: 1, date: "2021-09-16", mealType: "lunch", recipesId: [29, 30] },
   { id: 1, date: "2021-09-16", mealType: "afternoon_snack", recipesId: [12] },
-  { id: 1, date: "2021-09-16", mealType: "dinner", recipesId: [] },
+  { id: 1, date: "2021-09-16", mealType: "dinner", recipesId: [37] },
 
   { id: 1, date: "2021-09-17", mealType: "breakfast", recipesId: [1] },
   { id: 1, date: "2021-09-17", mealType: "morning_snack", recipesId: [11] },
-  { id: 1, date: "2021-09-17", mealType: "lunch", recipesId: [32] },
+  { id: 1, date: "2021-09-17", mealType: "lunch", recipesId: [36] },
   { id: 1, date: "2021-09-17", mealType: "afternoon_snack", recipesId: [12] },
-  { id: 1, date: "2021-09-17", mealType: "dinner", recipesId: [] }
+  { id: 1, date: "2021-09-17", mealType: "dinner", recipesId: [21] }
 ];
